a5e69ab208677f36d8cf18c16be4d39772b413a5
Ian Richardson <iantrich@gmail.com>
2021-06-12 09:18:52 -0500

    fix syntax error

--- group-card.js
+++ group-card.js
@@ -9,7 +9,7 @@ class GroupCard extends HTMLElement {
     const cardConfig = Object.assign({}, config);
     if (!cardConfig.card) cardConfig.card = {};
     if (!cardConfig.card.type) cardConfig.card.type = 'entities';
-    if (!cardConfig.entities_vars) cardConfig.entities_vars = {type: 'entity'};
+    if (!cardConfig.entities_vars) cardConfig.entities_vars = { type: 'entity' };
     const element = document.createElement(`hui-${cardConfig.card.type}-card`);
     this.appendChild(element);
     this._config = JSON.parse(JSON.stringify(cardConfig));
@@ -17,7 +17,7 @@ class GroupCard extends HTMLElement {
 
   set hass(hass) {
     const config = this._config;
-    if (!hass.states[config.group]){
+    if (!hass.states[config.group]) {
       throw new Error(`${config.group} not found`);
     }
     const entities = hass.states[config.group].attributes['entity_id'];
@@ -26,29 +26,31 @@ class GroupCard extends HTMLElement {
       if (!config.row) {
         config.card.entities = entities;
       }
-      else {      
+      else {
         const fmtentities = [];
-        entities.forEach (function (item) {
+        entities.forEach(function (item) {
           const stateObj = new Object;
           for (var k in config.row)
-            stateObj[k]=config.row[k];
+            stateObj[k] = config.row[k];
           stateObj.entity = item;
-          fmtentities.push (stateObj);
+          fmtentities.push(stateObj);
         });
         config.card.entities = fmtentities;
-      if (config.card.type == "entities" || config.card.type == "glance") {
-        config.card.entities = entities;
-      } else {
-        config.card.cards = []
-        for(const entity of entities){
-          const card = JSON.parse(JSON.stringify(config.entities_vars));
-          card.entity = entity
-          config.card.cards.push(card)
+        if (config.card.type == "entities" || config.card.type == "glance") {
+          config.card.entities = entities;
+        } else {
+          config.card.cards = []
+          for (const entity of entities) {
+            const card = JSON.parse(JSON.stringify(config.entities_vars));
+            card.entity = entity
+            config.card.cards.push(card)
+          }
         }
       }
+
+      this.lastChild.setConfig(config.card);
+      this.lastChild.hass = hass;
     }
-    this.lastChild.setConfig(config.card);
-    this.lastChild.hass = hass;
   }
 
   getCardSize() {
